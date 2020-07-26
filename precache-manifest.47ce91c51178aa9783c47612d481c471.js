957d91c42a2518ff94d7b8c1243a0b8f299787c4
Michael <mykhaylo88@icloud.com>
2020-07-26 16:56:29 +0300

    Updates

---
+++ precache-manifest.47ce91c51178aa9783c47612d481c471.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "737518f8087901da5fafc2a3f1b1fc44",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "3edc2d8c43c73185e9ed",
+    "url": "/goit-react-hw-01-components/static/css/main.74f2e416.chunk.css"
+  },
+  {
+    "revision": "8d9dc58c1a4c85358da3",
+    "url": "/goit-react-hw-01-components/static/js/2.3cf5bb85.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/static/js/2.3cf5bb85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3edc2d8c43c73185e9ed",
+    "url": "/goit-react-hw-01-components/static/js/main.2a95f911.chunk.js"
+  },
+  {
+    "revision": "7c1abe1df921e466788f",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.e943b22f.js"
+  }
+]);
